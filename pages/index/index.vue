d0d252e022d943dcd9d589e4299b3e48df352211
aibayanyu <aibayanyu@qq.com>
2021-08-13 08:37:37 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="title">
+		{{title}}
+	</view>
+</template>
+
+<script>
+	import { ref } from "vue"
+	export default {
+		setup(){
+			const title = ref("测试");
+			return {
+				title
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.title{
+		color: $y-color-primary;
+	}
+</style>
